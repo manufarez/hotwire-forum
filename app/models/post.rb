118c32714f9410046c33c969850394f32b6bac5e
manufarez <manufarez@gmail.com>
2022-11-29 16:14:54 -0500

    Real time creation of posts for everybody on discussion page

--- app/models/post.rb
+++ app/models/post.rb
@@ -3,6 +3,8 @@ class Post < ApplicationRecord
   belongs_to :user, default: -> { Current.user }
   has_rich_text :body
   validates :body, presence: true
+
+  after_create_commit -> { broadcast_append_to discussion, partial: "discussions/posts/post", post: self }
 end
 
 #Counter cache is just a database column storing the number of children, with the value automatically updated.
