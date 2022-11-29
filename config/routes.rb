c44c22bd219ff8897df1b7d32b7549e7414708a0
manufarez <manufarez@gmail.com>
2022-11-28 19:25:25 -0500

    Remove discussion

--- config/routes.rb
+++ config/routes.rb
@@ -2,7 +2,7 @@ Rails.application.routes.draw do
   get 'discussions/index'
   devise_for :users
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
-  resources :discussions, only: [:index, :create, :new, :edit, :update]
+  resources :discussions, only: [:index, :create, :new, :edit, :update, :destroy]
   # Defines the root path route ("/")
   root "main#index"
 end
