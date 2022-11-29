4077bb74d2832142147e219a61908338db6329a9
manufarez <manufarez@gmail.com>
2022-11-28 19:21:14 -0500

    Edit discussions

--- config/routes.rb
+++ config/routes.rb
@@ -2,7 +2,7 @@ Rails.application.routes.draw do
   get 'discussions/index'
   devise_for :users
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
-  resources :discussions, only: [:index, :create, :new]
+  resources :discussions, only: [:index, :create, :new, :edit, :update]
   # Defines the root path route ("/")
   root "main#index"
 end
