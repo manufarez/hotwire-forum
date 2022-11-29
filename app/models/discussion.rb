20a8995a8faba0e5de950101db7c51b643bfb746
manufarez <manufarez@gmail.com>
2022-11-29 09:30:46 -0500

    Realtime discussion functinality

--- app/models/discussion.rb
+++ app/models/discussion.rb
@@ -3,7 +3,12 @@ class Discussion < ApplicationRecord
 
   validates :name, presence: true
 
+  #This callback is called after a record has been created.
+  after_create_commit -> { broadcast_prepend_to "discussions" }
+  after_update_commit -> { broadcast_replace_to "discussions" }
+  after_destroy_commit -> { broadcast_remove_to "discussions" }
+
   def to_param
-    "#{id}-#{name.downcase.to_string[0..50]}".parameterize
+    "#{id}-#{name.downcase.to_str[0..50]}".parameterize
   end
 end
