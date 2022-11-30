68caba4151c3969325c0a6a2f50962a290c12a85
manufarez <manufarez@gmail.com>
2022-11-30 15:05:11 -0500

    Adding categories to discussion index

--- app/models/category.rb
+++ app/models/category.rb
@@ -3,4 +3,8 @@ class Category < ApplicationRecord
 
   #Scopes always return an ActiveRecord::Relation. A class method, returns anything you want.
   scope :sorted, -> { order(name: :asc) }
+
+  after_create_commit -> { broadcast_prepend_to 'categories' }
+  after_update_commit -> { broadcast_replace_to 'categories' }
+  after_destroy_commit -> { broadcast_remove_to 'categories' }
 end
