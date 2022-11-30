4026152abda2ebeb102dcb53097db8c48e0a8f1e
manufarez <manufarez@gmail.com>
2022-11-30 17:38:13 -0500

    Make discussions close in realtime

--- app/controllers/discussions_controller.rb
+++ app/controllers/discussions_controller.rb
@@ -34,8 +34,11 @@ class DiscussionsController < ApplicationController
   def update
     respond_to do |format|
       if @discussion.update(discussion_params)
+
+        #Make discussion index update in realtime
         @discussion.broadcast_replace(partial: "discussions/header", locals: {discussion: @discussion})
 
+        #Make discussion update its category in realtime
         if @discussion.saved_change_to_category_id?
           #Below is an implicit array of 2 variables -> old_category_id, old_category_id = [id_1, id_2]
           old_category_id, new_category_id = @discussion.saved_change_to_category_id
@@ -50,6 +53,17 @@ class DiscussionsController < ApplicationController
           #Update categories by replacing them. This updates the counters in the sidebar.
           old_category.reload.broadcast_replace_to("categories")
           new_category.reload.broadcast_replace_to("categories")
+        end
+
+        #Make discussion open/closed in realtime
+        if @discussion.saved_change_to_closed?
+          @discussion.broadcast_action_to(
+            @discussion,
+            action: :replace,
+            target: "new_post_form",
+            partial: "discussions/posts/form",
+            locals: {post: @discussion.posts.new}
+          )
         end
 
         format.html { redirect_to @discussion, notice: 'Discussion updated' }
